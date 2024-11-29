43259ef5c4a4a5e4982cd7128de0d88442a962b7
pratyush panwar <avipanwar1212@gmail.com>
2024-11-29 13:53:33 +0530

    version before deploying 2

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Person = require('./../models/Person');
+const Person = require('./../Models/Person');
 const { findByIdAndDelete } = require('../Models/MenuItem');
 
 
